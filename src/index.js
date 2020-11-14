44e15eb374a8f8134b6059e368d60887a72e7eec
Sophie Durand <sophie.durand.b@gmail.com>
2020-11-14 13:59:17 +0100

    removed old gradient

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import './less/index.less';
 import './less/footer.less';
 import './less/header.less';
 import './less/card.less';
-import './less/gradient.less';
 import './less/case-study.less';
 import './less/base.less';
